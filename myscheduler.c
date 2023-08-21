9c75a8b133fa66bee1f1f0e078bea9b19e753e58
Jacob Read <read.jacob3@outlook.com>
2023-08-21 21:26:32 +0800

    Update myscheduler.c

--- myscheduler.c
+++ myscheduler.c
@@ -39,31 +39,94 @@
 
 #define CHAR_COMMENT                    '#'
 
+struct device {
+    char name[MAX_DEVICE_NAME+1];
+    int read_speed;
+    int write_speed;
+};
+
 struct process {
+    struct command *command;
     int time;
     struct process **children;
     int num_children;
     struct process *parent;
 };
 
-void read_sysconfig(char argv0[], char filename[])
-{
+struct command {
+    char name[MAX_COMMAND_NAME+1];
+    struct syscall **syscalls;
+    int num_syscalls;
+};
+
+enum syscall_types {
+    SPAWN,
+    READ,
+    WRITE,
+    SLEEP,
+    WAIT,
+    EXIT
+};
+
+struct syscall {
+    int time;
+    enum syscall_types type;
+    struct device *device;
+    int data;
+};
+
+struct device *create_device(char *name, int read_speed, int write_speed) {
+    struct device *new_device = malloc(sizeof(struct device));
+    strcpy(new_device->name, name);
+    new_device->read_speed = read_speed;
+    new_device->write_speed = write_speed;
+    return new_device;
 }
 
-void read_commands(char argv0[], char filename[])
-{
+struct process *create_process(struct command *command, int time, struct process *parent) {
+    struct process *new_process = malloc(sizeof(struct process));
+    new_process->command = command;
+    new_process->time = time;
+    new_process->children = NULL;
+    new_process->num_children = 0;
+    new_process->parent = parent;
+    return new_process;
+}
+
+struct command *create_command(char *name, struct syscall **syscalls, int num_syscalls) {
+    struct command *new_command = malloc(sizeof(struct command));
+    strcpy(new_command->name, name);
+    new_command->syscalls = syscalls;
+    new_command->num_syscalls = num_syscalls;
+    return new_command;
+}
+
+struct syscall *create_syscall(int time, enum syscall_types type, struct device *device, int data) {
+    struct syscall *new_syscall = malloc(sizeof(struct syscall));
+    new_syscall->time = time;
+    new_syscall->type = type;
+    new_syscall->device = device;
+    new_syscall->data = data;
+    return new_syscall;
+}
+
+void read_sysconfig(char argv0[], char filename[]) {
+
+}
+
+void read_commands(char argv0[], char filename[]) {
+
 }
 
 //  ----------------------------------------------------------------------
 
-void execute_commands(void)
-{
+void execute_commands(void) {
+
 }
 
 //  ----------------------------------------------------------------------
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 //  ENSURE THAT WE HAVE THE CORRECT NUMBER OF COMMAND-LINE ARGUMENTS
     if(argc != 3) {
         printf("Usage: %s sysconfig-file command-file\n", argv[0]);
